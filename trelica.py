b54db10e78d505369ba7a3435d63f2c495146974
Hugo Tahara Menegatti <103003201+taharaLovelace@users.noreply.github.com>
2023-05-04 22:45:29 -0300

    Criação de Tabela dos Nós

--- trelica.py
+++ trelica.py
@@ -2,18 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-coluna = 'X Y RX RY FX FY '.split()
-linha = '1 2 3 4 5 6 7 8 9'.split()
-coordenax = []
+coordenadax = []
 coordenaday = []
+forcax = []
+forcay = []
 
+#entrada das coordenadas dos nós e forças da treliça
 n = int(input('Quantos nos tera na trelica? :'))
 
 for i in range(n):
   x = int(input('Insira a Coordenada X do No:'))
-  coordenax.append(x)
+  coordenadax.append(x)
   y = int(input('Insira a Coordenada Y do No:'))
   coordenaday.append(y)
+  fx = int(input('Insira a forca aplicada em FX neste no:'))
+  forcax.append(fx)
+  fy = int(input('Insira a forca aplicada em FY neste no:'))
+  forcay.append(fy)
 
-print(coordenax)
-print(coordenaday)
+tabela = pd.DataFrame({'X': coordenadax, 'Y': coordenaday, 'FX': forcax, 'FY': forcay})
+print(tabela)
+
+#calculo das forças da treliça
+
+
+
+
+
+
+
+
+#plotagem do gráfico que representa a treliça
