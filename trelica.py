9383c5221cf265b5b5d2f0c1f2d80f5c728ee525
Alexandre <aletoscaro@gmail.com>
2023-05-11 18:34:58 -0300

    adicoes tabela barra

--- trelica.py
+++ trelica.py
@@ -5,15 +5,19 @@ import os
  
 os.system('cls')
 
-#VETORES DECLARADOS DAS TABELAS
+# VETORES DECLARADOS DAS TABELAS
 coordenadax = []
 coordenaday = []
 forcax = []
 forcay = []
 barra1 = []
 barra2 = []
+Ls = []
+sens = []
+coss = []
 
-#INICIO DO PROGRAMA
+
+# INICIO DO PROGRAMA
 print("                                                 Seja bem-vindo(a) ao nosso Programa de Cálculo Estrutural.")
 print("           Para facilitar a execução do programa, imagine o sistema de treliças em forma de coordenadas cartesianas. Os eixos X e Y estão na escala de metros.")
 print("                                                             As forças estão na escala de 'kN'")
@@ -21,7 +25,7 @@ print("                                                             Presione 'en
 inicio=input("                                                                             ")
 os.system('cls')
 
-#ENTRADA DAS COORDENADAS DOS NÓS E FORÇAS APLICADAS EXTERNAS NA TRELIÇA
+# ENTRADA DAS COORDENADAS DOS NÓS E FORÇAS APLICADAS EXTERNAS NA TRELIÇA
 n = int(input('Quantos nós a trelica terá?: '))
 
 for i in range(n):
@@ -36,12 +40,13 @@ for i in range(n):
 
 tabelanos = pd.DataFrame({'X': coordenadax, 'Y': coordenaday, 'FX': forcax, 'FY': forcay})
 tabelanos.index += 1
-print(tabelanos)
+print("\nInformações dos nós da treliça:")
+print(tabelanos, "\n")
 
-#ENTRADA DAS BARRAS DA TRELIÇA (QUAIS NÓS SE LIGAM ENTRE SI)
+# ENTRADA DAS BARRAS DA TRELIÇA (QUAIS NÓS SE LIGAM ENTRE SI)
 print('Quais nós estarão interligados entre si? ')
 continuar = True
-#simulando um do-while
+# simulando um do-while
 while continuar:
     x1 = int(input('Insira o Primeiro Nó a ser ligado: '))
     barra1.append(x1)
@@ -54,10 +59,8 @@ while continuar:
 
 tabelabarras = pd.DataFrame({'N1': barra1, 'N2': barra2})
 tabelabarras.index += 1
-print(tabelabarras)
 
-#CÁLCULO DAS FORÇAS DA TRELIÇA
-
+# CÁLCULO DAS FORÇAS DA TRELIÇA
 
 
 
@@ -65,7 +68,8 @@ print(tabelabarras)
 
 
 
-#PLOTAGEM DO GRÁFICO QUE REPRESENTA A TRELIÇA
+
+# PLOTAGEM DO GRÁFICO QUE REPRESENTA A TRELIÇA
 plt.figure(1, figsize=(12,4.5))
 for barra in tabelabarras.index:
   N1, N2 = tabelabarras.loc[barra, ['N1', 'N2']]
@@ -76,7 +80,7 @@ for barra in tabelabarras.index:
   Y = np.array([y2, y3])
   plt.plot(X, Y, color='black')
 
-#IMPORTANDO DADOS DOS NÓS NO GRÁFICO
+# IMPORTANDO DADOS DOS NÓS NO GRÁFICO
 for no in tabelanos.index:
    X, Y, FX, FY = tabelanos.loc[no]
 
@@ -93,8 +97,36 @@ for no in tabelanos.index:
       plt.text(X, Y - 1.50, '{:.2f}kN'.format(FY/1000), va='bottom', rotation=90)
    if FY < 0:
       plt.arrow(X, Y + 1.50, 0, -1, color='red', width=0.05)
-      plt.text(X, Y + 1.50, '{:.2f}kN'.format(FY/1000), va='bottom', rotation=90)       
+      plt.text(X, Y + 1.50, '{:.2f}kN'.format(FY/1000), va='bottom', rotation=90) 
 
-#Após todo o pediodo de entrada de informações pelo usuário, exibe o grafico
+for barra in tabelabarras.index:
+   
+   N1 = tabelabarras.loc[barra, 'N1']
+   N2 = tabelabarras.loc[barra, 'N2']
+   
+   x1, y1 = tabelanos.loc[N1, ['X','Y']]
+   x2, y2 = tabelanos.loc[N2, ['X','Y']]
+   # O comprimento da barra é dado pelo teorema de Pitagoras
+   Lx = x2 - x1
+   Ly = y2 - y1    
+   L = np.sqrt(Lx**2 + Ly**2)
+   # Aplicando a lei do seno e cosseno:
+   sen = Ly/L
+   cos = Lx/L
+   # Inserindo nas listas
+   Ls.append(L)
+   sens.append(sen)
+   coss.append(cos)
+   
+# Inserindo as informações na tabela das barras
+tabelabarras['L'] = Ls
+tabelabarras['sen'] = sens
+tabelabarras['cos'] = coss
+
+print("\nInformações das barras da treliça:")
+print(tabelabarras, "\n") 
+
+# Após todo o pediodo de entrada de informações pelo usuário, exibe o grafico
+plt.title('Cálculo Estrutural de Treliças')
 plt.show()
 
