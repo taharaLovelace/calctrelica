176153ebd7071fa31636a371b822d50c9754f98d
Alexandre <aletoscaro@gmail.com>
2023-06-04 13:35:39 -0300

    correção de bugs e melhorias

--- trelica.py
+++ trelica.py
@@ -32,11 +32,15 @@ os.system('cls')
 while True:  # validação entrada de quantos nós
     entrada = input('Quantos nós a trelica terá?: ')
     if entrada.isdigit():
+        if entrada == '0':
+            print("Entrada inválida, fim do programa!!!!")
+            os._exit(0)
         n = int(entrada)
         break
     else:
         print("Entrada inválida. Tente novamente.")
 print("\n")
+
 
 # ENTRADA DAS COORDENADAS DOS NÓS E FORÇAS APLICADAS EXTERNAS NA TRELIÇA
 for i in range(n):
@@ -121,268 +125,272 @@ while continuar:
 tabelabarras = pd.DataFrame({'N1': barra1, 'N2': barra2})
 tabelabarras.index += 1
 
+try:
+    # PLOTAGEM DO GRÁFICO QUE REPRESENTA A TRELIÇA
+    plt.figure(1, figsize=(12, 4.5))
+    for barra in tabelabarras.index:
+        N1, N2 = tabelabarras.loc[barra, ['N1', 'N2']]
+        x2, y2 = tabelanos.loc[N1, ['X', 'Y']]
+        x3, y3 = tabelanos.loc[N2, ['X', 'Y']]
 
-# PLOTAGEM DO GRÁFICO QUE REPRESENTA A TRELIÇA
-plt.figure(1, figsize=(12, 4.5))
-for barra in tabelabarras.index:
-    N1, N2 = tabelabarras.loc[barra, ['N1', 'N2']]
-    x2, y2 = tabelanos.loc[N1, ['X', 'Y']]
-    x3, y3 = tabelanos.loc[N2, ['X', 'Y']]
+        X = np.array([x2, x3])
+        Y = np.array([y2, y3])
+        plt.plot(X, Y, color='black')
 
-    X = np.array([x2, x3])
-    Y = np.array([y2, y3])
-    plt.plot(X, Y, color='black')
+    # IMPORTANDO DADOS DOS NÓS NO GRÁFICO
+    for no in tabelanos.index:
+        X, Y, FX, FY, RX, RY = tabelanos.loc[no]
 
-# IMPORTANDO DADOS DOS NÓS NO GRÁFICO
-for no in tabelanos.index:
-    X, Y, FX, FY, RX, RY = tabelanos.loc[no]
+        plt.scatter(X, Y, s=50, color='grey', marker="o")
 
-    plt.scatter(X, Y, s=50, color='grey', marker="o")
+        if RX == 1:
+            plt.scatter(X, Y, 400, marker=5, zorder=-2, color='blue')
+        if RY == 1:
+            plt.scatter(X, Y, 400, marker=6, zorder=-2, color='blue')
 
-    if RX == 1:
-        plt.scatter(X, Y, 400, marker=5, zorder=-2, color='blue')
-    if RY == 1:
-        plt.scatter(X, Y, 400, marker=6, zorder=-2, color='blue')
+        if FX > 0:
+            plt.arrow(X - 1.5, Y, 1, 0, color='red', width=0.05)
+            plt.text(X - 1.5, Y, '{:.2f}kN'.format(FX / 1000), va='bottom')
+        if FX < 0:
+            plt.arrow(X + 1.5, Y, -1, 0, color='red', width=0.05)
+            plt.text(X + 0.50, Y, '{:.2f}kN'.format(FX / 1000), va='bottom')
+        if FY > 0:
+            plt.arrow(X, Y - 1.50, 0, 1, color='red', width=0.05)
+            plt.text(X, Y + 0.50, '{:.2f}kN'.format(FY / 1000), va='bottom', rotation=90)
+        if FY < 0:
+            plt.arrow(X, Y + 1.50, 0, -1, color='red', width=0.05)
+            plt.text(X, Y + 0.50, '{:.2f}kN'.format(FY / 1000), va='bottom', rotation=90)
 
-    if FX > 0:
-        plt.arrow(X - 1.5, Y, 1, 0, color='red', width=0.05)
-        plt.text(X - 1.5, Y, '{:.2f}kN'.format(FX / 1000), va='bottom')
-    if FX < 0:
-        plt.arrow(X + 1.5, Y, -1, 0, color='red', width=0.05)
-        plt.text(X + 0.50, Y, '{:.2f}kN'.format(FX / 1000), va='bottom')
-    if FY > 0:
-        plt.arrow(X, Y - 1.50, 0, 1, color='red', width=0.05)
-        plt.text(X, Y + 0.50, '{:.2f}kN'.format(FY / 1000), va='bottom', rotation=90)
-    if FY < 0:
-        plt.arrow(X, Y + 1.50, 0, -1, color='red', width=0.05)
-        plt.text(X, Y + 0.50, '{:.2f}kN'.format(FY / 1000), va='bottom', rotation=90)
+    for barra in tabelabarras.index:
+        N1 = tabelabarras.loc[barra, 'N1']
+        N2 = tabelabarras.loc[barra, 'N2']
 
-for barra in tabelabarras.index:
-    N1 = tabelabarras.loc[barra, 'N1']
-    N2 = tabelabarras.loc[barra, 'N2']
-
-    x1, y1 = tabelanos.loc[N1, ['X', 'Y']]
-    x2, y2 = tabelanos.loc[N2, ['X', 'Y']]
-    # O comprimento da barra é dado pelo teorema de Pitagoras
-    Lx = x2 - x1
-    Ly = y2 - y1
-    L = np.sqrt(Lx ** 2 + Ly ** 2)
-    # Aplicando a lei do seno e cosseno:
-    sen = Ly / L
-    cos = Lx / L
-    # Inserindo nas listas
-    Ls.append(L)
-    sens.append(sen)
-    coss.append(cos)
-    As.append(0.01)             #CARACTERÍSTICAS DAS BARRAS A e E PARA A MONTAGEM DA MATRIZ DE RIGIDEZ
-    Es.append(210000000000)     # módulo de elasticidade do aço estrutural
-
-
-# Inserindo as novas informações na tabela das barras
-tabelabarras['L'] = Ls
-tabelabarras['sen'] = sens
-tabelabarras['cos'] = coss
-tabelabarras['A'] = As
-tabelabarras['E'] = Es
-
-print("\nInformações das barras da treliça:")
-print(tabelabarras, "\n")
-
-# CÁLCULO DAS FORÇAS DA TRELIÇA
-
-maxgl = 2 * len(tabelanos.index)  # aloca previamente uma matriz de acordo com o numero de nós da treliça
-K = np.zeros([maxgl, maxgl])
-
-for barra in tabelabarras.index:
-    N1, N2, L, sen, cos, A, E = tabelabarras.loc[barra]
-
-    #Matriz de Rigidez Local
-    Kl = E * A / L * np.array([[1, 0, -1, 0],
-                               [0, 0, 0, 0],
-                               [-1, 0, 1, 0],
-                               [0, 0, 0, 0]])
-
-    # Matriz de rotação
-    Mrot = np.array([[cos, sen, 0, 0],
-                     [-sen, cos, 0, 0],
-                     [0, 0, cos, sen],
-                     [0, 0, -sen, cos]])
-
-    Klr = np.dot(np.dot(Mrot.T, Kl), Mrot)
-
-    #Graus de liberdade
-    gl1 = int(2 * N1 - 1)       #é necessário o int pois como os gls serão usados
-    gl2 = int(2 * N1)           #para index, nao podem possuir ponto flutuante
-    gl3 = int(2 * N2 - 1)
-    gl4 = int(2 * N2)
-
-    #Construção da Matriz Global de Rigidez
-    K[gl1 - 1:gl2, gl1 - 1:gl2] += Klr[0:2, 0:2]
-    K[gl3 - 1:gl4, gl1 - 1:gl2] += Klr[2:4, 0:2]
-    K[gl1 - 1:gl2, gl3 - 1:gl4] += Klr[0:2, 2:4]
-    K[gl3 - 1:gl4, gl3 - 1:gl4] += Klr[2:4, 2:4]
-#MATRIZ PARA O CÁLCULO DAS REAÇÕES DE APOIO
-K2 = K.copy()  # Alocando a matriz em outro espaço de memória
-for no in tabelanos.index:
-    RX, RY = tabelanos.loc[no, ['RX', 'RY']]
-    gl1 = int(2 * no - 1)
-    gl2 = int(2 * no)
-
-    if RX == 1:
-        K2[:, gl1 - 1] = 0
-        K2[gl1 - 1, :] = 0
-        K2[gl1 - 1, gl1 - 1] = 1
+        x1, y1 = tabelanos.loc[N1, ['X', 'Y']]
+        x2, y2 = tabelanos.loc[N2, ['X', 'Y']]
+        # O comprimento da barra é dado pelo teorema de Pitagoras
+        Lx = x2 - x1
+        Ly = y2 - y1
+        L = np.sqrt(Lx ** 2 + Ly ** 2)
+        # Aplicando a lei do seno e cosseno:
+        sen = Ly / L
+        cos = Lx / L
+        # Inserindo nas listas
+        Ls.append(L)
+        sens.append(sen)
+        coss.append(cos)
+        As.append(0.01)             #CARACTERÍSTICAS DAS BARRAS A e E PARA A MONTAGEM DA MATRIZ DE RIGIDEZ
+        Es.append(210000000000)     # módulo de elasticidade do aço estrutural
 
 
-    if RY == 1:
-        K2[:, gl2 - 1] = 0
-        K2[gl2 - 1, :] = 0
-        K2[gl2 - 1, gl2 - 1] = 1
+    # Inserindo as novas informações na tabela das barras
+    tabelabarras['L'] = Ls
+    tabelabarras['sen'] = sens
+    tabelabarras['cos'] = coss
+    tabelabarras['A'] = As
+    tabelabarras['E'] = Es
 
-#CONSTRUÇÃO DO VETOR DE FORÇAS
-F = np.zeros([maxgl])
+    print("\nInformações das barras da treliça:")
+    print(tabelabarras, "\n")
 
-for no in tabelanos.index:
-    FX, FY = tabelanos.loc[no, ['FX', 'FY']]
-    gl1 = int(2 * no - 1)
-    gl2 = int(2 * no)
+    # CÁLCULO DAS FORÇAS DA TRELIÇA
 
-    if FX != 0:
-        F[gl1-1] = FX           #-1 para corrigir a posição do index
-    if FY != 0:
-        F[gl2-1] = FY
-D = np.linalg.solve(K2, F)
-R = np.dot(K, D)
-print(R)
+    maxgl = 2 * len(tabelanos.index)  # aloca previamente uma matriz de acordo com o numero de nós da treliça
+    K = np.zeros([maxgl, maxgl])
 
-# PLOTAGEM DO GRÁFICO QUE REPRESENTA A TRELIÇA
-plt.figure(2, figsize=(12, 4.5))
-for barra in tabelabarras.index:
-    N1, N2 = tabelabarras.loc[barra, ['N1', 'N2']]
-    x2, y2 = tabelanos.loc[N1, ['X', 'Y']]
-    x3, y3 = tabelanos.loc[N2, ['X', 'Y']]
+    for barra in tabelabarras.index:
+        N1, N2, L, sen, cos, A, E = tabelabarras.loc[barra]
 
-    X = np.array([x2, x3])
-    Y = np.array([y2, y3])
-    plt.plot(X, Y, color='black')
+        #Matriz de Rigidez Local
+        Kl = E * A / L * np.array([[1, 0, -1, 0],
+                                [0, 0, 0, 0],
+                                [-1, 0, 1, 0],
+                                [0, 0, 0, 0]])
 
-# IMPORTANDO DADOS DOS NÓS NO GRÁFICO
-for no in tabelanos.index:
-    X, Y, RX, RY = tabelanos.loc[no, ['X','Y','RX','RY']]
-    plt.scatter(X, Y, s=50, color='grey', marker="o")
-    gl1 = int(2 * no - 1)
-    gl2 = int(2 * no)
+        # Matriz de rotação
+        Mrot = np.array([[cos, sen, 0, 0],
+                        [-sen, cos, 0, 0],
+                        [0, 0, cos, sen],
+                        [0, 0, -sen, cos]])
 
-    if RX == 1:
-        reacX = R[gl1-1]
+        Klr = np.dot(np.dot(Mrot.T, Kl), Mrot)
 
-        plt.arrow(X - 1.5, Y, 1, 0, color='red', width=0.05)
-        plt.text(X - 1.5, Y, '{:.2f}kN'.format(reacX / 1000), va='bottom')
-    if RY == 1:
-        reacY =R[gl2-1]
-        plt.arrow(X, Y - 1.50, 0, 1, color='red', width=0.05)
-        plt.text(X, Y + 0.50, '{:.2f}kN'.format(reacY / 1000), va='bottom', rotation=90)
+        #Graus de liberdade
+        gl1 = int(2 * N1 - 1)       #é necessário o int pois como os gls serão usados
+        gl2 = int(2 * N1)           #para index, nao podem possuir ponto flutuante
+        gl3 = int(2 * N2 - 1)
+        gl4 = int(2 * N2)
 
-# Determinação dos esforços nas barras
-Esf = []
+        #Construção da Matriz Global de Rigidez
+        K[gl1 - 1:gl2, gl1 - 1:gl2] += Klr[0:2, 0:2]
+        K[gl3 - 1:gl4, gl1 - 1:gl2] += Klr[2:4, 0:2]
+        K[gl1 - 1:gl2, gl3 - 1:gl4] += Klr[0:2, 2:4]
+        K[gl3 - 1:gl4, gl3 - 1:gl4] += Klr[2:4, 2:4]
+    #MATRIZ PARA O CÁLCULO DAS REAÇÕES DE APOIO
+    K2 = K.copy()  # Alocando a matriz em outro espaço de memória
+    for no in tabelanos.index:
+        RX, RY = tabelanos.loc[no, ['RX', 'RY']]
+        gl1 = int(2 * no - 1)
+        gl2 = int(2 * no)
 
-for barra in tabelabarras.index:
-    N1, N2, L, sen, cos, A, E = tabelabarras.loc[barra]
-    
-    # Matriz de rigidez no sistema local 
-    Kl = E*A/L*np.array([[ 1, 0,-1, 0],
-                         [ 0, 0, 0, 0], 
-                         [-1, 0, 1, 0],
-                         [ 0, 0, 0, 0]])
-
-    # Matriz de rotação
-    Mrot = np.array([[ cos,  sen,    0,   0],
-                     [-sen,  cos,    0,   0],
-                     [    0,   0,  cos, sen],
-                     [    0,   0, -sen, cos]])
-    
-    # Determinação dos gls
-    gl1 = int(2*N1-1)
-    gl2 = int(2*N1)
-    gl3 = int(2*N2-1)
-    gl4 = int(2*N2)
-    
-    # Capturar os deslocamentos
-    Dlg = np.zeros([4])
-    Dlg[0] = D[gl1-1]
-    Dlg[1] = D[gl2-1]
-    Dlg[2] = D[gl3-1]
-    Dlg[3] = D[gl4-1]
-    
-    # Rotaciona Dlg
-    Dl = np.dot(Mrot, Dlg)
-    
-    # Determina esforços no sentido da barra 
-    Fl = np.dot(Kl, Dl)
-    FAx = Fl[2]
-    Esf.append(FAx)
-# Colocando Esf no dataframe de barras
-tabelabarras['Esf'] = Esf
-tabelabarras
-# Colocando deslocamentos nodais em Nós
-Dx = []
-Dy = []
-
-for no in tabelanos.index:
-    gl1 = int(2*no-1)
-    gl2 = int(2*no)
-    
-    Dx.append(D[gl1-1])
-    Dy.append(D[gl2-1])
-
-tabelanos['Dx'] = Dx
-tabelanos['Dy'] = Dy 
-
-tabelanos
-
-# Plotando esforços nas barras
-plt.figure(3, figsize=(12,3))
-plt.title('Esforços axiais em kN')
-
-for barra in tabelabarras.index:
-    N1, N2, Esf, sen, cos = tabelabarras.loc[barra, ['N1', 'N2', 'Esf', 'sen', 'cos']]
-    x1, y1 = tabelanos.loc[N1, ['X', 'Y']]
-    x2, y2 = tabelanos.loc[N2, ['X', 'Y']]
-    
-    if cos != 0:
-        tg = sen/cos
-        ang = np.arctan(tg)
-        ang = 180*ang/np.pi
-    else:
-        ang = 90
-    
-    x = [x1, x2]
-    y = [y1, y2]
-    
-    if Esf == 0:
-        cor = 'k'
-    elif Esf > 0:
-        cor = 'r'
-    else:
-        # Esf < 0
-        cor = 'b'
-    plt.plot(x, y, cor, zorder=-1)
-    
-    plt.text(np.mean(x), np.mean(y),
-             '{:.2f}kN'.format(Esf/1000),
-             rotation=ang,
-             horizontalalignment='center',
-             verticalalignment='center',
-             size = 14,
-             weight ='bold'
-            )
-    
-    # Desenhando rotulas
-    plt.scatter(x, y, s=40, color='black', marker="o", zorder=0) # 6 é restrição vertical e 5 na horizontal
+        if RX == 1:
+            K2[:, gl1 - 1] = 0
+            K2[gl1 - 1, :] = 0
+            K2[gl1 - 1, gl1 - 1] = 1
 
 
-# Após todo o pediodo de entrada de informações pelo usuário, exibe o grafico
-plt.figure(1, figsize=(12, 4.5))  # Criando a figura e definindo seu número e tamanho em X e Y
-plt.grid(True)  # Plotando a grade
-plt.title('Cálculo Estrutural de Treliças')
-plt.show()
+        if RY == 1:
+            K2[:, gl2 - 1] = 0
+            K2[gl2 - 1, :] = 0
+            K2[gl2 - 1, gl2 - 1] = 1
+
+    #CONSTRUÇÃO DO VETOR DE FORÇAS
+    F = np.zeros([maxgl])
+
+    for no in tabelanos.index:
+        FX, FY = tabelanos.loc[no, ['FX', 'FY']]
+        gl1 = int(2 * no - 1)
+        gl2 = int(2 * no)
+
+        if FX != 0:
+            F[gl1-1] = FX           #-1 para corrigir a posição do index
+        if FY != 0:
+            F[gl2-1] = FY
+    D = np.linalg.solve(K2, F)
+    R = np.dot(K, D)
+    print(R)
+
+    # PLOTAGEM DO GRÁFICO QUE REPRESENTA A TRELIÇA
+    plt.figure(2, figsize=(12, 4.5))
+    for barra in tabelabarras.index:
+        N1, N2 = tabelabarras.loc[barra, ['N1', 'N2']]
+        x2, y2 = tabelanos.loc[N1, ['X', 'Y']]
+        x3, y3 = tabelanos.loc[N2, ['X', 'Y']]
+
+        X = np.array([x2, x3])
+        Y = np.array([y2, y3])
+        plt.plot(X, Y, color='black')
+
+    # IMPORTANDO DADOS DOS NÓS NO GRÁFICO
+    for no in tabelanos.index:
+        X, Y, RX, RY = tabelanos.loc[no, ['X','Y','RX','RY']]
+        plt.scatter(X, Y, s=50, color='grey', marker="o")
+        gl1 = int(2 * no - 1)
+        gl2 = int(2 * no)
+
+        if RX == 1:
+            reacX = R[gl1-1]
+
+            plt.arrow(X - 1.5, Y, 1, 0, color='red', width=0.05)
+            plt.text(X - 1.5, Y, '{:.2f}kN'.format(reacX / 1000), va='bottom')
+        if RY == 1:
+            reacY =R[gl2-1]
+            plt.arrow(X, Y - 1.50, 0, 1, color='red', width=0.05)
+            plt.text(X, Y + 0.50, '{:.2f}kN'.format(reacY / 1000), va='bottom', rotation=90)
+
+    # Determinação dos esforços nas barras
+    Esf = []
+
+    for barra in tabelabarras.index:
+        N1, N2, L, sen, cos, A, E = tabelabarras.loc[barra]
+        
+        # Matriz de rigidez no sistema local 
+        Kl = E*A/L*np.array([[ 1, 0,-1, 0],
+                            [ 0, 0, 0, 0], 
+                            [-1, 0, 1, 0],
+                            [ 0, 0, 0, 0]])
+
+        # Matriz de rotação
+        Mrot = np.array([[ cos,  sen,    0,   0],
+                        [-sen,  cos,    0,   0],
+                        [    0,   0,  cos, sen],
+                        [    0,   0, -sen, cos]])
+        
+        # Determinação dos gls
+        gl1 = int(2*N1-1)
+        gl2 = int(2*N1)
+        gl3 = int(2*N2-1)
+        gl4 = int(2*N2)
+        
+        # Capturar os deslocamentos
+        Dlg = np.zeros([4])
+        Dlg[0] = D[gl1-1]
+        Dlg[1] = D[gl2-1]
+        Dlg[2] = D[gl3-1]
+        Dlg[3] = D[gl4-1]
+        
+        # Rotaciona Dlg
+        Dl = np.dot(Mrot, Dlg)
+        
+        # Determina esforços no sentido da barra 
+        Fl = np.dot(Kl, Dl)
+        FAx = Fl[2]
+        Esf.append(FAx)
+    # Colocando Esf no dataframe de barras
+    tabelabarras['Esf'] = Esf
+    tabelabarras
+    # Colocando deslocamentos nodais em Nós
+    Dx = []
+    Dy = []
+
+    for no in tabelanos.index:
+        gl1 = int(2*no-1)
+        gl2 = int(2*no)
+        
+        Dx.append(D[gl1-1])
+        Dy.append(D[gl2-1])
+
+    tabelanos['Dx'] = Dx
+    tabelanos['Dy'] = Dy 
+
+    tabelanos
+
+    # Plotando esforços nas barras
+    plt.figure(3, figsize=(12,3))
+    plt.title('Esforços axiais em kN')
+
+    for barra in tabelabarras.index:
+        N1, N2, Esf, sen, cos = tabelabarras.loc[barra, ['N1', 'N2', 'Esf', 'sen', 'cos']]
+        x1, y1 = tabelanos.loc[N1, ['X', 'Y']]
+        x2, y2 = tabelanos.loc[N2, ['X', 'Y']]
+        
+        if cos != 0:
+            tg = sen/cos
+            ang = np.arctan(tg)
+            ang = 180*ang/np.pi
+        else:
+            ang = 90
+        
+        x = [x1, x2]
+        y = [y1, y2]
+        
+        if Esf == 0:
+            cor = 'k'
+        elif Esf > 0:
+            cor = 'r'
+        else:
+            # Esf < 0
+            cor = 'b'
+        plt.plot(x, y, cor, zorder=-1)
+        
+        plt.text(np.mean(x), np.mean(y),
+                '{:.2f}kN'.format(Esf/1000),
+                rotation=ang,
+                horizontalalignment='center',
+                verticalalignment='center',
+                size = 14,
+                weight ='bold'
+                )
+        
+        # Desenhando rotulas
+        plt.scatter(x, y, s=40, color='black', marker="o", zorder=0) # 6 é restrição vertical e 5 na horizontal
+
+    print("\n\n\n                                                             Presione 'enter' para prosseguir!\n")
+    final = input("                                                                             ")
+    # Após todo o pediodo de entrada de informações pelo usuário, exibe o grafico
+    plt.figure(1, figsize=(12, 4.5))  # Criando a figura e definindo seu número e tamanho em X e Y
+    plt.grid(True)  # Plotando a grade
+    plt.title('Cálculo Estrutural de Treliças')
+    plt.show()
+except Exception as e:
+    print("Treliça com ausencia de equilibiro estatico\n\n\n ")
+    os._exit(0)
